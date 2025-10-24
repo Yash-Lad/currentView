e757813d6e2b96302a53f603230597e31fda1347
Yash Lad <yashjmlad@gmail.com>
2025-10-24 14:48:59 -0500

    Updated Files

--- src/services/newsServices.js
+++ src/services/newsServices.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Get API key from environment variables
 const API_KEY= import.meta.env.VITE_NEWS_API_KEY
-const BASE_URL= import.meta.env.DEV ? "/api" : "https://newsapi.org/v2"
+const BASE_URL= "https://newsapi.org/v2"
 
 // Create axios instance with base configuration for News API
 export const newsAPI= axios.create({
